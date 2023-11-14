c0c1113a5231e1dfc9eacf666dbf5ded43262608
Abdur Rehman Kazim <abdurrehmankazim68@gmail.com>
2023-11-15 03:36:26 +0500

    deletion left

--- trees.cpp
+++ trees.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <stack>
 #include <vector>
+#include <queue>
+#include <string>
 using namespace std;
 
 class Node {
@@ -100,15 +102,288 @@ class Tree{
             
             return result;
         }
+
+        // breadth first approach
+        // iterative approach
+        vector<string> breadthFirstValuesIterative(){
+            vector<string> result;
+            queue<Node*> q;
+
+            q.push(root);
+
+            while(!q.empty()){
+                Node* current = q.front();
+                q.pop();
+
+                if (current->left){
+                    q.push(current->left);
+                }
+                if (current->right){
+                    q.push(current->right);
+                }
+
+                result.push_back(current->data);
+            }
+
+            return result;
+        }
+
+        bool treeIncludes(string value){
+            if (!root) return false;
+
+            queue<Node*> q;
+            q.push(root);
+
+            while(!q.empty()){
+                Node *current = q.front();
+                q.pop();
+
+                if (current->data == value){
+                    return true;
+                }
+
+                if (current->left){
+                    q.push(current->left);
+                }
+
+                if (current->right){
+                    q.push(current->right);
+                }
+            }
+
+            return false;
+        }
+
+        // recursive approach
+        bool treeIncludesRecursive(Node* current, string value){
+            if (!current) return false;
+
+            if (current->data == value){
+                return true;
+            }
+
+            return treeIncludesRecursive(current->left, value) || treeIncludesRecursive(current->right, value);
+        }
+
+        // calculate sum
+        // iterative approach
+        int sumIterative(){
+            if (!root)
+                return 0;
+            
+            queue<Node*> q;
+            q.push(root);
+
+            int sum = 0;
+            Node* current = root;
+
+            while (!q.empty()){
+                current = q.front();
+                q.pop();
+
+                sum += stoi(current->data);
+
+                if (current->left){
+                    q.push(current->left);
+                }
+
+                if (current->right){
+                    q.push(current->right);
+                }
+
+            }
+
+            return sum;
+        }
+
+        // recursive approach
+        int sumRecursive(Node* current){
+            if (!current){
+                return 0;
+            }
+            return stoi(current->data) + sumRecursive(current->left) + sumRecursive(current->right);
+        }
+
+        // find max
+        // iterative approach
+        int maxIterative(){
+            if (!root){
+                return 0;
+            }
+
+            queue<Node *> q;
+            q.push(root);
+
+            int max = INT16_MIN;
+
+            while (!q.empty()){
+                Node *current = q.front();
+
+                if (stoi(current->data) > max){
+                    max = stoi(current->data);
+                }
+
+                q.pop();
+
+                if (current->left){
+                    q.push(current->left);
+                }
+
+                if (current->right){
+                    q.push(current->right);
+                }
+
+            }
+
+            return max;
+        }
+
+        // recursive approach
+        int maxRecursive(Node* current){
+            if (!current){
+                return INT16_MIN;
+            }
+
+            int max = stoi(current->data);
+            int leftMax = maxRecursive(current->left);
+            int rightMax = maxRecursive(current->right);
+
+            if (leftMax > max){
+                max = leftMax;
+            }
+
+            if (rightMax > max){
+                max = rightMax;
+            }
+
+            return max;
+        }
+
+        // find min
+        // iterative approach
+        int minIterative(){
+            if (!root){
+                return 0;
+            }
+            
+            int min = INT16_MAX;
+
+            queue<Node*> q;
+            q.push(root);
+
+            while (!q.empty()){
+                Node* current = q.front();
+
+                if (stoi(current->data) < min){
+                    min = stoi(current->data);
+                }
+
+                q.pop();
+
+                if (current->left){
+                    q.push(current->left);
+                }
+
+                if (current->right){
+                    q.push(current->right);
+                }
+            }
+
+            return min;
+        }
+
+        // recursive approach
+        int minRecursive(Node* current){
+            if (!current){
+                return INT16_MAX;
+            }
+
+            int min = stoi(current->data);
+            int leftMin = minRecursive(current->left);
+            int rightMin = minRecursive(current->right);
+
+            if (leftMin < min){
+                min = leftMin;
+            }
+
+            if (rightMin < min){
+                min = rightMin;
+            }
+
+            return min;
+        }
+
+        // max sum of a path
+        // recursive approach
+        int maxSum(Node* current){
+            if (!current){
+                return 0;
+            }
+            if (!current->left && !current->right){
+                return stoi(current->data);
+            }
+
+            int leftMax = maxSum(current->left);
+            int rightMax = maxSum(current->right);
+
+            if (leftMax > rightMax){
+                return stoi(current->data) + leftMax;
+            } else {
+                return stoi(current->data) + rightMax;
+            }
+        }
+
+        // inserting into binary search tree
+        // iterative approach
+        Node* insertIterative(string val){
+            if (!root){
+                return new Node(val);
+            }
+
+            Node* temp = root;
+
+            while(temp){
+                if (stoi(val) > stoi(temp->data)){
+                    if (!temp->right){
+                        temp->right = new Node(val);
+                        return root;
+                    }
+                    temp = temp->right;
+                }
+                else{
+                    if (!temp->left){
+                        temp->left = new Node(val);
+                        return root;
+                    }
+                    temp = temp->left;
+                }
+            }
+        }
+
+        // recursive approach
+        Node* insertRecursive(Node* current, string val){
+            if (!current){
+                return new Node(val);
+            }
+
+            if (stoi(val) > stoi(current->data)){
+                current->right = insertRecursive(current->right, val);
+            } else {
+                current->left = insertRecursive(current->left, val);
+            }
+
+            return current;
+        }
+
 };
 
 int main(){
-    Node* a = new Node("a");
-    Node *b = new Node("b");
-    Node *c = new Node("c");
-    Node *d = new Node("d");
-    Node *e = new Node("e");
-    Node *f = new Node("f");
+    Node* a = new Node("1");
+    Node *b = new Node("2");
+    Node *c = new Node("3");
+    Node *d = new Node("4");
+    Node *e = new Node("5");
+    Node *f = new Node("6");
 
     a->left = b;
     a->right = c;
@@ -124,12 +399,13 @@ int main(){
 
     Tree t(a);
 
-    vector<string> temp = t.depthFirstValuesRecursive(t.root);
+    t.insertRecursive(t.root, "7");
 
-    for (int i = 0; i < temp.size(); i++){
-        cout << temp[i] << "-->";
+    vector<string> result = t.breadthFirstValuesIterative();
+
+    for (int i = 0; i < result.size(); i++){
+        cout << result[i] << "-->";
     }
-
 
     return 0;
 }
